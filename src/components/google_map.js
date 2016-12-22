1301e7a2c33cf00b7a843386f87c20f9152bce81
ckim16 <aero.cheewoon@gmail.com>
2016-12-22 11:22:33 -0800

    add css

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,10 +1,15 @@
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
 import Map, { Marker } from 'google-maps-react';
+import ReactTooltip from 'react-tooltip';
 
 export default class GoogleMap extends Component {
   constructor(props) {
     super(props);
     this.renderTrucks = this.renderTrucks.bind(this);
+  }
+
+  onMouseoverMarker(props, marker, e) {
+    console.log('this', this.name);
   }
 
   renderTrucks() { 
@@ -13,16 +18,21 @@ export default class GoogleMap extends Component {
     }   
 
     return this.props.list.map((truckInfo) => {
-      return <Marker
-        key={truckInfo.objectid} 
+      return (<Marker
+        key={truckInfo.objectid}
+        name={truckInfo.applicant}
+        onMouseover={this.onMouseoverMarker} 
         position={{lat:truckInfo.latitude, lng:truckInfo.longitude}} />
+      );
     });
   }
+
   
   render() {
 
     return (
       <Map google={window.google}
+        className={'map'}
         style={{width: '50%', height: '80%', position: 'relative'}}
         zoom={13}>
         {this.renderTrucks()}
