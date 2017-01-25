cb4ffed25f1347a1e6157dbba39f03801a382e57
ckim16 <aero.cheewoon@gmail.com>
2017-01-25 00:34:15 -0800

    get path dependency and update server.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
 
 app.listen(port, function() {
   console.log('Server listening on port, ', port);
