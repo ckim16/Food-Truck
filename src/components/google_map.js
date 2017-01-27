82a11cfbd18dcda233e450b7cfecfd5810199b9d
ckim16 <aero.cheewoon@gmail.com>
2017-01-26 18:11:37 -0800

    remove google-maps-react and start using pure google map

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,22 +1,56 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Map, { Marker, InfoWindow } from 'google-maps-react';
+// import Map, { Marker, InfoWindow } from 'google-maps-react';
 
 export default class GoogleMap extends Component {
   constructor(props) {
     super(props);
-
+    console.log('hey', props);
     this.state = {
       showingInfoWindow: false,
       activeMarker: {},
       selectedPlace: {}
     };
 
-    this.renderTrucks = this.renderTrucks.bind(this);
-    this.onMarkerClick = this.onMarkerClick.bind(this);
-    this.onMapClicked = this.onMapClicked.bind(this);
+    // this.renderTrucks = this.renderTrucks.bind(this);
+    // this.onMarkerClick = this.onMarkerClick.bind(this);
+    // this.onMapClicked = this.onMapClicked.bind(this);
   }
 
+  componentDidMount(newProps) {
+    const latLong = {lat: 37.742972, lng: -122.431297};
+
+    const options = {
+            zoom: 13,
+            center: latLong
+          },
+          map = new google.maps.Map(ReactDOM.findDOMNode(this), options);
+    //     marker = new google.maps.Marker({
+    //     map:map,
+    //     animation: google.maps.Animation.DROP,
+    //     position: latLong
+    // });
+  }
+
+  // componentWillMount() {
+  //   console.log('this', this.props.list);
+  //   if (this.props.list.length !== 0) {
+  //     return this.prop.list.map((truck) => {
+  //       return new google.maps.Marker({
+  //         map: map,
+  //         position: new google.maps.LatLng({lat: truck.latitude, long: truck.longitude})
+  //       });
+  //     });  
+  //   } 
+  // }
+
+  render() {
+    return (
+      <div className="map"></div>
+    );
+  }
+
+  // ----------------------------------------------------------------
   // componentWillMount() {
   //   document.addEventListener('mouseover', this.handleHover, false);
   // }
@@ -39,86 +73,86 @@ export default class GoogleMap extends Component {
   //   }
   // }
   
-  onMarkerClick(props, marker, e) {
-    console.log('p', props, 'm', marker, 'e2', e);
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        activeMarker: null,
-        showingInfoWindow: false
-      });
-    } else { 
-      this.setState({
-        selectedPlace: props,
-        activeMarker: marker,
-        showingInfoWindow: true
-      });
-    }
-  } 
+  // onMarkerClick(props, marker, e) {
+  //   console.log('p', props, 'm', marker, 'e2', e);
+  //   if (this.state.showingInfoWindow) {
+  //     this.setState({
+  //       activeMarker: null,
+  //       showingInfoWindow: false
+  //     });
+  //   } else { 
+  //     this.setState({
+  //       selectedPlace: props,
+  //       activeMarker: marker,
+  //       showingInfoWindow: true
+  //     });
+  //   }
+  // } 
 
-  onMapClicked(props) {
-    console.log('map click', props);
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      });
-    }
-  }
+  // onMapClicked(props) {
+  //   console.log('map click', props);
+  //   if (this.state.showingInfoWindow) {
+  //     this.setState({
+  //       showingInfoWindow: false,
+  //       activeMarker: null
+  //     });
+  //   }
+  // }
 
-  renderTrucks() {
-    // assign marker with id
+  // renderTrucks() {
+  //   // assign marker with id
     
-    if (this.props.filteredList.length !== 0 || !this.props.filteredList) {
-      return this.props.filteredList.map((filteredTruck) => {
-        return (<Marker
-          key={filteredTruck.objectid}
-          name={filteredTruck.applicant}
-          address={filteredTruck.address}
-          hours={filteredTruck.dayshours}
-          items={filteredTruck.fooditems}
-          onClick={this.onMarkerClick}
-          icon={'https://maxcdn.icons8.com/windows8/PNG/26/Transport/truck-26.png'}
-          animation={'drop'}
-          position={{lat:filteredTruck.latitude, lng:filteredTruck.longitude}} />
-        );
-      });
-    } else {
-      return this.props.list.map((truckInfo) => {
-        return (<Marker
-          key={truckInfo.objectid}
-          name={truckInfo.applicant}
-          address={truckInfo.address}
-          hours={truckInfo.dayshours}
-          items={truckInfo.fooditems}
-          onClick={this.onMarkerClick}
-          animation={google.maps.Animation.BOUNCE}
-          position={{lat:truckInfo.latitude, lng:truckInfo.longitude}} />
-        );
-      });
-    }
-  }
+  //   if (this.props.filteredList.length !== 0 || !this.props.filteredList) {
+  //     return this.props.filteredList.map((filteredTruck) => {
+  //       return (<Marker
+  //         key={filteredTruck.objectid}
+  //         name={filteredTruck.applicant}
+  //         address={filteredTruck.address}
+  //         hours={filteredTruck.dayshours}
+  //         items={filteredTruck.fooditems}
+  //         onClick={this.onMarkerClick}
+  //         icon={'https://maxcdn.icons8.com/windows8/PNG/26/Transport/truck-26.png'}
+  //         animation={'drop'}
+  //         position={{lat:filteredTruck.latitude, lng:filteredTruck.longitude}} />
+  //       );
+  //     });
+  //   } else {
+  //     return this.props.list.map((truckInfo) => {
+  //       return (<Marker
+  //         key={truckInfo.objectid}
+  //         name={truckInfo.applicant}
+  //         address={truckInfo.address}
+  //         hours={truckInfo.dayshours}
+  //         items={truckInfo.fooditems}
+  //         onClick={this.onMarkerClick}
+  //         animation={google.maps.Animation.BOUNCE}
+  //         position={{lat:truckInfo.latitude, lng:truckInfo.longitude}} />
+  //       );
+  //     });
+  //   }
+  // }
 
   
-  render() {
-    return (
-      <Map google={window.google}
-        className={'map'}
-        style={{width: '60vw', height: '100vh'}}
-        onClick={this.onMapClicked}
-        zoom={13}>
-        {this.renderTrucks()}
+  // render() {
+  //   return (
+  //     <Map google={window.google}
+  //       className={'map'}
+  //       style={{width: '60vw', height: '100vh'}}
+  //       onClick={this.onMapClicked}
+  //       zoom={13}>
+  //       {this.renderTrucks()}
 
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}>
-          <p>
-            {this.state.selectedPlace.name}<br /> 
-            {this.state.selectedPlace.address}<br /> 
-            {this.state.selectedPlace.hours}<br />
-            {this.state.selectedPlace.items}
-          </p>
-        </InfoWindow>        
-      </Map>
-    );
-  }
+  //       <InfoWindow
+  //         marker={this.state.activeMarker}
+  //         visible={this.state.showingInfoWindow}>
+  //         <p>
+  //           {this.state.selectedPlace.name}<br /> 
+  //           {this.state.selectedPlace.address}<br /> 
+  //           {this.state.selectedPlace.hours}<br />
+  //           {this.state.selectedPlace.items}
+  //         </p>
+  //       </InfoWindow>        
+  //     </Map>
+  //   );
+  // }
 }
