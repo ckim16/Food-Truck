3ea1c139094d492548fc9b4ec4ca093492415464
ckim16 <aero.cheewoon@gmail.com>
2017-01-30 23:25:14 -0800

    add props to the map component

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -17,25 +17,23 @@ export default class GoogleMap extends Component {
   }
 
   loadMap() {
-    console.log('google',this.props);
     if (this.props && this.props.google) {
       const { google } = this.props;
       const maps = google.maps;
-      console.log('maps', maps);
       const mapRef = this.refs.map;
       const node = ReactDOM.findDOMNode(mapRef);
 
-      const zoom = 13;
-      let lat = 37.742972;
-      let lng = -122.431297;
+      // let zoom = 13;
+      // let lat = 37.763972;
+      // let lng = -122.431297;
+      let {initialCenter, zoom} = this.props;
+      const {lat, lng} = initialCenter;
       const center = new maps.LatLng(lat, lng);
-      console.log('center', center);
       const mapConfig = Object.assign({}, {
         center: center,
         zoom: zoom
       });
       this.map = new maps.Map(node, mapConfig);
-      console.log('inside', this.map);
     }
   }
 
@@ -43,5 +41,20 @@ export default class GoogleMap extends Component {
     return (
       <div ref="map" className="map"></div>
     );
+  }
+}
+
+GoogleMap.propTypes = {
+  google: React.PropTypes.object,
+  zoom: React.PropTypes.number,
+  initialCenter: React.PropTypes.object
+}
+
+GoogleMap.defaultProps = {
+  zoom: 13,
+  // San Francisco, by default
+  initialCenter: {
+    lat: 37.763972,
+    lng: -122.431297
   }
 }
