d45775b0118ad26ee4452f0043061d843462142f
ckim16 <aero.cheewoon@gmail.com>
2017-05-11 22:19:00 -0700

    remove axios

--- src/components/app.js
+++ src/components/app.js
@@ -8,26 +8,26 @@ import TruckList from '../containers/truck_list';
 const url = `https://data.sfgov.org/resource/6a9r-agq8.json`;
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { trucks: [] };
-    this.allTrucks = [];
-    axios.get(url)
-    .then((truckLists) => {
-      truckLists.data.map((truck) => {
-        this.allTrucks.push(truck);
-      });
-      this.setState({ trucks: this.allTrucks });
-    });
-  }
+  // constructor(props) {
+  //   super(props);
+  //
+  //   this.state = { trucks: [] };
+  //   this.allTrucks = [];
+  //   axios.get(url)
+  //   .then((truckLists) => {
+  //     truckLists.data.map((truck) => {
+  //       this.allTrucks.push(truck);
+  //     });
+  //     this.setState({ trucks: this.allTrucks });
+  //   });
+  // }
 
   render() {
     return (
       <div className="app">
-        <SearchBar /> 
-        <TruckMap allTrucks={this.state.trucks}/>
-        <TruckList allTrucks={this.state.trucks} />
+        <SearchBar />
+        <TruckMap />
+        <TruckList />
       </div>
     );
   }
