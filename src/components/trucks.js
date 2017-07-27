5bc4b809062800c162a9c631fadc6eb0d00b444a
ckim16 <aero.cheewoon@gmail.com>
2017-07-26 23:22:13 -0700

    change marker to fontawesome truck

--- src/components/trucks.js
+++ src/components/trucks.js
@@ -4,14 +4,14 @@ import { greatPlaceStyle, greatPlaceStyleHover } from '../helper/truck_style';
 
 export default class Trucks extends Component {
   render() {
-    const style = this.props.hover ? greatPlaceStyleHover : greatPlaceStyle;
+    // const style = this.props.hover ? greatPlaceStyleHover : greatPlaceStyle;
+    const style = this.props.hover ? {color: 'red', cursor: 'poiner'} : {color: 'blue', cursor: 'pointer'};
     return (
-      <div className="hint--html hint--hoverable hint--top" style={style}>
-          <div>{this.props.block}</div>
+      <i className="fa fa-truck fa2x" style={style}>
           <div style={{width: 80}} className="hint__content">
             ?
           </div>
-       </div>
+       </i>
     );
   }
 }
